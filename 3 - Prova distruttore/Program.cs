8bfb49615d7a3473f67a92cfa3a197f371311978
corsi.20188 <corsi.20188@E2-9-PC26>
2023-09-26 13:46:52 +0200

    fine

--- 3 - Prova distruttore/Program.cs
+++ 3 - Prova distruttore/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Classe3;
 
 namespace _3___Prova_distruttore
 {
@@ -10,6 +11,13 @@ namespace _3___Prova_distruttore
     {
         static void Main(string[] args)
         {
+
+            Esempio esempio = new Esempio("stringa", 14);
+
+            Console.WriteLine($"La stringa è: {esempio.Stringa}");
+            Console.WriteLine($"Il numero è: {esempio.Numero}");
+
+            //esempio.Dispose();
         }
     }
 }
--- Classe3/Class1.cs
+++ Classe3/Class1.cs
@@ -9,4 +9,57 @@ namespace Classe3
     public class Class1
     {
     }
+    public class Esempio : IDisposable
+    {
+        // attributi
+        private string stringa;
+        private double numero;
+        private bool disposed = false;
+
+        //costruttore 
+        public Esempio(string stringa, double numero)
+        {
+            this.stringa = stringa;
+            this.numero = numero;
+        }
+
+        // metodi accessor
+        public string Stringa
+        {
+            get { return stringa; }
+            set { stringa = value; }
+        }
+
+        public double Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                
+            }
+
+            disposed = true;
+        }
+
+        ~Esempio()
+        {
+            Console.WriteLine("Distruttore");
+            Dispose(false);
+
+            
+        }
+    }
 }
